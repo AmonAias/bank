d9dcfea94b74424cc64da4a61fcccdd36736a5bb
Danil <danilswwwwsse@gmail.com>
2025-07-21 19:48:51 +0300

    check app by linters

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,4 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """
     Маскирует номер банковской карты.
 
@@ -10,7 +10,7 @@ def get_mask_card_number(card_number: int) -> str:
     return f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """
     Маскирует номер банковского счета.
 
